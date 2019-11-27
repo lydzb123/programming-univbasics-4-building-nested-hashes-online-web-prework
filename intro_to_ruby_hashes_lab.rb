68524d1a1be5bdddae6913e085c97fb268f3d7f5
Lydia B <lydiabaik@gmail.com>
2019-11-27 08:32:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,12 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	:railroads => {:pieces => 4, :rent_in_dollars => [25, 50, 75, 100]}
+	:railroads => {:pieces => 4, :rent_in_dollars => {
+		:one_piece_owned => 25,
+		:two_pieces_owned => 50,
+		:three_pieces_owned => 100,
+		:four_pieces_owned => 200
+		}}
 	:railroads
 end
 
